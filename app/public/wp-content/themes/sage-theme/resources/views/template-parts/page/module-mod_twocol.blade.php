5680ac63cfa452b1d21c90b37dd2574a37e3e468
NamWRC <nam.lh.61cntt@ntu.edu.vn>
2022-12-06 22:50:38 +0700

    6/12/2022

--- app/public/wp-content/themes/sage-theme/resources/views/template-parts/page/module-mod_twocol.blade.php
+++ app/public/wp-content/themes/sage-theme/resources/views/template-parts/page/module-mod_twocol.blade.php
@@ -1,17 +1,17 @@
 <section class="mod-twocol w-full flex justify-center">
     <div class="flex lg:justify-center w-4/5 lg:flex-row xl:w-[60%] flex-col items-center lg:items-start">
         <div class="main-tittle lg:max-w-[428px] lg:w-2/5 md:mr-0 lg:mr-[88px] mb-[67px] ">
-            <div class="line-4"></div>
-            <h1 class="text-h1 w-full text-[36px]">
+            <div class="line-4 h-[2px] mb-[29px]"></div>
+            <h2>
                 The Encompass Platform Seamlessly Connects the Supply
                 Chain, Allowing You to:
-            </h1>
+            </h2>
         </div>
-        <div class="info-wraper flex w-full lg:w-3/5 mt-[7px]">
+        <div class="info-wraper flex w-full lg:w-3/5 mt-[7px] lg:max-w-[634px]">
             <div class="row-content">
-                <div class="info">
-                    <h2 class="tittle text-[24px]">Drive Business Growth</h2>
-                    <p  class="content font-[300] text-[18px]">
+                <div class="info lg:max-w-[273px] lg:max-w-[634px] md:mr-[8%]">
+                    <h3>Drive Business Growth</h3>
+                    <p >
                     Tap into the power of predictive analytics and demand 
                     forecasting. Capitalize on revenue-generating 
                     opportunities faster.  Capture mission-critical data 
@@ -20,12 +20,12 @@
                     food business into new levels of profitability. 
                     </p>
                 </div>
-                <div class="info">
-                    <h2 class="tittle text-[24px]">
+                <div class="info lg:max-w-[273px] lg:max-w-[634px]">
+                    <h3>
                         Leverage Real-Time Data for Better
                         Decision-Making
-                    </h2>
-                    <p class="content font-[300] text-[18px]">
+                    </h3>
+                    <p>
                         Accelerate business decisions with better, more
                         accurate system-to-system data and comprehensive
                         reporting that results in deep, actionable
@@ -36,9 +36,9 @@
                 </div>
             </div>
             <div class="row-content mt-[33px]">
-                <div class="info">
-                    <h2 class="tittle text-[24px]">Improve Operational efficiencies</h2>
-                    <p class="content font-[300] text-[18px] ">
+                <div class="info lg:max-w-[273px] lg:max-w-[634px] md:mr-[8%]">
+                    <h3>Improve Operational efficiencies</h3>
+                    <p>
                         Streamline processes and save money by reducing
                         leakage with a fully integrated ERP system that
                         works for your business. Encompass ERP makes it
@@ -47,9 +47,9 @@
                         most optimal outcome.
                     </p>
                 </div>
-                <div class="info">
-                    <h2 class="tittle text-[24px]">Enhance Team Dynamics & Employee</h2>
-                    <p class="content font-[300] text-[18px]">
+                <div class="info lg:max-w-[273px] lg:max-w-[634px]">
+                    <h3>Enhance Team Dynamics & Employee</h3>
+                    <p>
                         When your labor force has the tools they need to
                         succeed, the workplace runs like a well-oiled
                         machine. With Encompass ERP, every stakeholder
