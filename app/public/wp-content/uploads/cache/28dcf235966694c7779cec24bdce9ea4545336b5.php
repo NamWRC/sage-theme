fa13e15c1d2b0427b9c2588ab604ced48c1223aa
NamWRC <nam.lh.61cntt@ntu.edu.vn>
2022-12-07 19:08:17 +0700

    7/12/2022

---
+++ app/public/wp-content/uploads/cache/28dcf235966694c7779cec24bdce9ea4545336b5.php
@@ -0,0 +1,23 @@
+<section class="mod-banner">
+    <div class="relative flex min-h-[857px]">
+        <svg class="z-0 absolute" viewBox="0 0 500 220" preserveAspectRatio="xMinYMin meet">
+            <path d="M0,220 C150,100 350,250 500,110 L500,00 L0,0 Z" style="stroke: none; fill:#b94a02; opacity:0.5"></path>
+        </svg>
+        <svg class="z-0 absolute" viewBox="0 0 500 190" preserveAspectRatio="xMinYMin meet">
+            <path d="M0,190 C150,200 350,170 500,110 L500,00 L0,0 Z" style="stroke: none; fill:#b94a02; opacity:0.5"></path>
+        </svg>
+        <svg class="z-0 absolute" viewBox="0 0 500 200" preserveAspectRatio="xMinYMin meet">
+            <path d="M0,200 C150,130 350,200 500,110 L500,00 L0,0 Z" style="stroke: none; fill:#b94a02;"></path>
+        </svg>
+        <div class="z-10 banner-content w-full">
+            <div class="banner-slogan text-slate-100">
+                <h1 class="text-slate-100">Unlock the Power of Connection</h1>
+                <p>Connect & Grow Your Business with the Power of the Encompass Platform</p>
+                <a href="">ABOUT US</a>
+            </div>
+            <div class="banner-img min-w-[900] min-h-[800px]">
+
+            </div>
+        </div>
+    </div>
+</section>
